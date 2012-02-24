633a4d207f3bf4167373631f2eb0316626773fb4
Joe Hudson <joehud@gmail.com>
2012-02-24 18:16:29 -0500

    added test case javadoc

--- rsr-json/src/test/java/test/org/rsr/JsonBeanTest.java
+++ rsr-json/src/test/java/test/org/rsr/JsonBeanTest.java
@@ -24,6 +24,10 @@ public class JsonBeanTest extends TestCase {
 		handler.addController(new Controller());
 	}
 
+	/**
+	 * Ensure that we can map a route to a controller method which returns a bean.
+	 * The SimpleJsonBeanSerializer converts the bean to JSON.
+	 */
 	public void testSimpleBean() throws Exception {
 		RestResponse response = handler.onRoute("/person/1234/Joe/Hudson",
 				new TestContext(), "get");
@@ -34,6 +38,12 @@ public class JsonBeanTest extends TestCase {
 				new String(out.toByteArray()));
 	}
 
+	/**
+	 * Ensure that we can map a route to a controller method which returns a bean.
+	 * The SimpleJsonBeanSerializer converts the bean to JSON.  This additional test
+	 * ensures that we can have nested beans (see the "deep" serializer annotation property
+	 * in the controller method below)
+	 */
 	public void testNestedBean() throws Exception {
 		RestResponse response = handler.onRoute("/addr/1234/123 Oak St./Seattle/Wa/12345",
 				new TestContext(), "get");
@@ -44,9 +54,16 @@ public class JsonBeanTest extends TestCase {
 				new String(out.toByteArray()));
 	}
 
+	/**
+	 * This would be the servlet in a traditional usage
+	 */
 	public class Controller implements Serializable {
 		private static final long serialVersionUID = 1L;
 
+		/**
+		 * Take person attributes identified in the route and return a fully constructed person.
+		 * Respond to the provided route and use the SimpleJsonBeanSerializer to convert to JSON
+		 */
 		@GET
 		@Path("/person/:id/:firstName/:lastName")
 		@Serializer(SimpleJsonBeanSerializer.class)
@@ -58,6 +75,11 @@ public class JsonBeanTest extends TestCase {
 			return p;
 		}
 
+		/**
+		 * Take person attributes identified in the route and return a fully constructed person.
+		 * Respond to the provided route and use the SimpleJsonBeanSerializer to convert to JSON
+		 * Allow for a nested address with the "deep=2" serializer parameter
+		 */
 		@GET
 		@Path("/addr/:id/:street/:city/:state/:zip")
 		@Serializer(type=SimpleJsonBeanSerializer.class, properties="deep=2")
@@ -74,6 +96,9 @@ public class JsonBeanTest extends TestCase {
 		}
 	}
 
+	/**
+	 * Simple person model object
+	 */
 	public static class Person implements Serializable {
 		private static final long serialVersionUID = 1L;
 
@@ -115,6 +140,9 @@ public class JsonBeanTest extends TestCase {
 		}
 	}
 
+	/**
+	 * Simple address model object
+	 */
 	public static class Address implements Serializable {
 		private String street1;
 		private String city;
